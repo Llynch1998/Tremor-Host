5220da489e681d0151dd43bce17fab199b2f96aa
Llynch1998 <llynch1998@gmail.com>
2019-11-10 14:37:23 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -19,6 +21,6 @@ io.on('connection', function(socket){
     });
 })
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log(`listening on *:${port}`);
 });
