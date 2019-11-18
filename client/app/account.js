54ccef7425614bf1fecde2d47df69e1c83192ae2
mpaseltiner <mjp3591@rit.edu>
2019-11-18 10:34:57 -0500

    password change window started

--- client/app/account.js
+++ client/app/account.js
@@ -4,40 +4,40 @@ const handlePasswordChange = (e) => {
     return false;
 }
 
-// const ChangePasswordWindow = (props) => {
-//     return(
-//         <form id="changePasswordForm" name="changePasswordForm"
-//             onSubmit={handleSignup}
-//             action="/passwordChange"
-//             method="POST"
-//             className="mainForm"
-//         >
-//             <label htmlFor="oldPassword">Old Password</label>
-//             <input id="oldPass" type="text" name="oldPassword" placeholder="current password"/>
-//             <label htmlFor="newPass">Password</label>
-//             <input id="newPass" type="password" name="newPass" placeholder="password"/>
-//             <label htmlFor="newPass2">Password</label>
-//             <input id="newPass2" type="password" name="newPass2" placeholder="retype password"/>
-//             <input type="hidden" name="_csrf" value={props.csrf}/>
-//             <input className="formSubmit" type="submit" value="Change Password" />
+const ChangePasswordWindow = (props) => {
+    return(
+        <form id="changePasswordForm" name="changePasswordForm"
+            onSubmit={handleSignup}
+            action="/passwordChange"
+            method="POST"
+            className="mainForm"
+        >
+            <label htmlFor="oldPassword">Old Password</label>
+            <input id="oldPass" type="text" name="oldPassword" placeholder="current password"/>
+            <label htmlFor="newPass">Password</label>
+            <input id="newPass" type="password" name="newPass" placeholder="password"/>
+            <label htmlFor="newPass2">Password</label>
+            <input id="newPass2" type="password" name="newPass2" placeholder="retype password"/>
+            <input type="hidden" name="_csrf" value={props.csrf}/>
+            <input className="formSubmit" type="submit" value="Change Password" />
 
-//         </form>
+        </form>
 
-//     )
-// }
+    )
+}
 
-// const createChangePasswordWindow = (csrf) => {
-//     ReactDOM.render(
-//         <ChangePasswordWindow csrf={csrf} />,
-//         document.querySelector("#content")
-//     );
-// }
+const createChangePasswordWindow = (csrf) => {
+    ReactDOM.render(
+        <ChangePasswordWindow csrf={csrf} />,
+        document.querySelector("#content")
+    );
+}
 
 const AccountInfo = (props) => {
     return (
         <form id="accountInfo" name="accountInfo"
             onSumit={handlePasswordChange}
-            action="/passwordChange"
+            action="/passChange"
             method="POST"
         >
             <p id="usernameLabel"><strong>Username:</strong> {props.username}</p>
@@ -49,9 +49,17 @@ const AccountInfo = (props) => {
 };
 
 const setup = function(csrf){
+    const changePasswordButton = document.querySelector("#changePasswordButton"); 
+
     ReactDOM.render(
-        <AccountInfo csrf={csrf} username={document.querySelector('#username').value}/>, document.querySelector('#account')
+        <AccountInfo csrf={csrf} username={document.querySelector('#username').value}/>, document.querySelector('#content')
     );
+
+    changePasswordButton.addEventListener("click", e => {
+        e.preventDefault();
+        createChangePasswordWindow(csrf);
+        return false;
+    })
         
 };
 
