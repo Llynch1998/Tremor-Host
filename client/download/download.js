979e284df23e7f0e19d3e6206c118b44a460ef7d
mpaseltiner <mjp3591@rit.edu>
2019-12-09 22:09:11 -0500

    downloads

--- client/download/download.js
+++ client/download/download.js
@@ -4,8 +4,8 @@ const DownloadPage = () => {
             <h2 id="downloadTitle">Download Tremor</h2>
             <p id="downloadDescription">The downloads contain a .zip file with the electron executable for Tremor. To run the app you need to unzip the folder and run the tremor.exe file.</p>
             <div id="downloadLinks">
-                <a id="windowsDownload" href="../hosted/downloads/TremorApp_Windows.zip" download>Download for Windows</a>
-                <a id="macDownload" href="" download>Download for MacOS</a>
+                <a id="windowsDownload" href="../hosted/downloads/TremorWindows.zip" download>Download for Windows</a>
+                <a id="macDownload" href="../hosted/download/TremorMac.zip" download>Download for MacOS</a>
             </div>
         </div>
         
